c0261fba72534fb26126ffbcaa1cb0f4d8eee862
Nadun Sameera Bandara <bandarans2000@gmail.com>
2026-06-13 06:22:04 +0530

    update scheduler to run hourly

--- routes/api.php
+++ routes/api.php
@@ -14,12 +14,12 @@ Route::post('/user-login', [AuthController::class, 'userLogin']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout-user', [AuthController::class, 'logout']);
 
-    // Todo CRUD
     Route::get('todos', [TodoController::class, 'index']);
     Route::post('/store-todo', [TodoController::class, 'store']);
     Route::get('todos/{id}', [TodoController::class, 'show']);
     Route::put('todos/{id}', [TodoController::class, 'update']);
     Route::delete('todos/{id}', [TodoController::class, 'destroy']);
+
     Route::patch('todos/{id}/complete', [TodoController::class, 'markComplete']);
     Route::patch('todos/{id}/pending', [TodoController::class, 'markPending']);
 
